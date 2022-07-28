7bec17bfc95837077ae5912dcb4c5c59060088a3
EvanKluger <jakeykluger@gmail.com>
2022-07-28 00:22:13 -0400

    fixed bugs

--- src/Components/Topbar/Topbar.jsx
+++ src/Components/Topbar/Topbar.jsx
@@ -4,7 +4,6 @@ import React from "react"
 export default function Topbar({menuOpen, setMenuOpen}) {
     return (
         <div className = {'topbar '+ (menuOpen && 'active')}>
-        TopBar: Name, Logo, Menu Bar....
         </div>
     )
 }
