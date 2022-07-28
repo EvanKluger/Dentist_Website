cd7bd486d3fcd3b9e8b2d88d86a9f1f342957e81
EvanKluger <jakeykluger@gmail.com>
2022-07-28 09:42:32 -0400

    fixed bugs

--- src/Components/Topbar/Topbar.jsx
+++ src/Components/Topbar/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar({menuOpen, setMenuOpen}) {
         <div className = {'topbar '+ (menuOpen && 'active')}>
            <div className="wrapper">
                 <div className="left">
-                <a href="#intro" className='logo'>Dr. Reena Rothman</a>
+                <a href="#intro" className='logo'>Dr. Reena Rothman - Dentist</a>
                 </div>
                     <div className="right">
                         <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
