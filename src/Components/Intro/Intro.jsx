5b145086620c60254532e0bd0ab9593dec0aa0f5
EvanKluger <jakeykluger@gmail.com>
2022-07-28 14:58:48 -0400

    Added Form with Email

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -6,7 +6,23 @@ export default function Intro() {
 
     return (
         <div className = 'intro' id='intro'>
-        Intro: Picture, Consultation Form, 
+            <div className='left'>
+                    <div className="imageContainer">
+                        <img src="assets/ReenaProfile.png" alt="" />
+                    </div>
+                </div>
+
+            <div className='right'>
+                <div className="wrapper">
+                <div className='signup'>Sign up here for a Consultation</div>
+                <form action="https://formsubmit.co/jakeykluger@gmail.com" method = 'POST'>
+                    <input type='text' name = 'name' placeholder = 'Name' required />
+                    <input type='email' name = 'email' placeholder = 'Email Address'required/>
+                    <button type = 'submit'>Submit</button>
+
+                </form>
+                </div> 
+            </div>
         </div>
     )
 }
