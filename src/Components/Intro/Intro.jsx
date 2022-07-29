e0c1b6c766f6f3923adb21564375bf77929eee4b
EvanKluger <jakeykluger@gmail.com>
2022-07-29 16:31:55 -0400

    fixed bugs

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -13,8 +13,8 @@ export default function Intro() {
                 </div>
 
             <div className='right'>
-                <div className="wrapper">
-                <div className='signup'>Hi, please fill out the form below </div>
+                <div className='signup'>
+                Hi, please fill out the form below
                 <form className = 'form' action="https://formsubmit.co/reenarothman@gmail.com" method = 'POST'>
                     <input classname = 'name' type='text' name = 'first name' placeholder = 'First Name' required/>
                     <input classname = 'email' type='email' name = 'email' placeholder = 'Email Address' required/>
@@ -22,9 +22,8 @@ export default function Intro() {
                     <input classname = 'message' type='message' name = 'message' placeholder = 'Tell me about yourself and what you are trying to address in your consultation'required/>
 
                     <button type = 'submit'>Submit</button>
-
                 </form>
-                </div> 
+                </div>
             </div>
         </div>
     )
