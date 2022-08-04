dcf6b8f82c41b65e1944818d838e8ceeef57e633
EvanKluger <jakeykluger@gmail.com>
2022-08-04 12:54:49 -0400

    Fixed bugs

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -11,34 +11,34 @@ export default function Gallery() {
         {
             id: 1,
             title: 'Image 1',
-            img: 'public\\assets\\dentist-jpg.png',
+            img: "assets/michael.png",
             decription: 'This will be Image 1',
         },
         
         {
             id: 2,
             title: 'Image 2',
-            img: 'public\\assets\\image-example.png',
+            img: "assets/michael.png",
             decription: 'This will be Image 2',
         },
         
         {
             id: 3,
             title: 'Image 3',
-            img: 'public\\assets\\image-example.png',
+            img: "assets/michael.png",
             decription: 'This will be Image 3',
         },
         
         {
             id: 4,
             title: 'Image 4',
-            img: 'public\\assets\\image-example.png',
+            img: "assets/michael.png",
             decription: 'Image 4',
         },
         {
             id: 5,
             title: 'Image 5',
-            img: 'public\\assets\\image-example.png',
+            img: "assets/michael.png",
             decription: 'Image 5',
         },
 
@@ -56,13 +56,6 @@ export default function Gallery() {
                 {data.map((d) => (
                 <div className="container">
                     <div className="item">
-                        <div className="left">
-                            <div className="leftContainer">
-    
-                                <h2>{d.title}</h2>
-                                <p>{d.decription}</p>
-                            </div>
-                        </div>
                         <div className="right">
                             <img src={d.img} alt="" />
                         </div>
